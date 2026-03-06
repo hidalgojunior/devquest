6b2142f31df20678f767d904887e732dde702bee
root <root@srv1159265.hstgr.cloud>
2026-03-06 07:15:49 +0000

    Translate configuration keys to Portuguese labels

--- resources/views/configurations/index.blade.php
+++ resources/views/configurations/index.blade.php
@@ -17,8 +17,21 @@
             </thead>
             <tbody>
                 @foreach($configs as $cfg)
+                @php
+                    $labels = [
+                        'point_presence' => 'Ponto por presença',
+                        'point_on_time' => 'Ponto por entrega no prazo',
+                        'point_missed' => 'Ponto por não entrega',
+                        'point_late' => 'Ponto por entrega atrasada',
+                        'point_occurrence' => 'Ponto por ocorrência',
+                        'point_late_15' => 'Ponto por atraso 15d',
+                        'point_late_30' => 'Ponto por atraso 30d',
+                        // add others if exist
+                    ];
+                    $label = $labels[$cfg->key] ?? $cfg->key;
+                @endphp
                 <tr class="odd:bg-white even:bg-gray-50">
-                    <td class="py-2 px-4">{{ $cfg->key }}</td>
+                    <td class="py-2 px-4">{{ $label }}</td>
                     <td class="py-2 px-4">{{ $cfg->value }}</td>
                     <td class="py-2 px-4">
                         <a href="{{ route('configurations.edit',$cfg) }}" class="inline-block bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium py-1 px-3 rounded-md transition">
